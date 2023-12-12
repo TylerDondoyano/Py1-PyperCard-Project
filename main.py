f1c6eddb66671c9688ef1fd5475a79e018ba4b79
Dondoyano_Tyler <dondt692@hsd.k12.or.us>
2023-12-12 15:21:08 -0800

    Completed the project

--- main.py
+++ main.py
@@ -7,33 +7,55 @@ name of the next card.
 from pypercard import App, Card
 
 
-def auto_func(app, card):
-    """
-    Called while transitioning from card 2, to card 3.
-    """
-    count = app.datastore.setdefault("counter", 0)
-    count += 1
-    app.datastore["counter"] = count
-    return "card3"
-
-
 # The templates for these cards can be found in pypercard.html.
 cards = [
-    Card("card1", auto_advance=10, transition="card2"),
-    Card("card2", auto_advance=20, transition=auto_func),
-    Card("card3", auto_advance=5, transition="card1"),
+    Card("card1"),
+    Card("card2"),
+    Card("card3"),
+    Card("card4"),
+    Card("card5"),
+    Card("card6"),
 ]
 
-
-# Create the app while ensuring the counter is reset.
+# Create the app
 carousel_app = App(
-    name="PyperCard carousel", datastore={"counter": 0}, cards=cards
+    name="PyperCard carousel", cards=cards
 )
 
+@carousel_app.transition("card1", "click", "Yes")
+def Yes(app, card):
+    return "card2"
 
-@carousel_app.transition("card2", "click", "reset")
-def reset(app, card):
+@carousel_app.transition("card1", "click", "No")
+def No(app, card):
+    return "card3"
+
+@carousel_app.transition("card2", "click", "Work_alone")
+def Work_by_yourself(app, card):
+    return "card4"
+
+@carousel_app.transition("card4", "click", "Return")
+def Return(app, card):
     return "card1"
 
+@carousel_app.transition("card2", "click", "Cheat")
+def Cheat(app, card):
+    return "card5"
+
+@carousel_app.transition("card5", "click", "Return")
+def Return(app, card):
+    return "card1"
+
+@carousel_app.transition("card3", "click", "Attempt_project")
+def Attempt_project(app, card):
+    return "card2"
+
+@carousel_app.transition("card3", "click", "Never_do_project")
+def Never_do_project(app, card):
+    return "card6"
+
+@carousel_app.transition("card6", "click", "Return")
+def Return(app, card):
+    return "card1"
 
 carousel_app.start("card1")
